5535603b75304789f9bfd0e0b94e5c41076b699b
Horváth Csongor <csongor@csbig.cslabs.local>
2018-01-20 16:49:09 +0100

    add QuizQuestion class

---
+++ app/src/main/java/com/example/csongor/quizapp/AnswerOption.java
@@ -0,0 +1,10 @@
+package com.example.csongor.quizapp;
+
+/**
+ * Created by csongor on 1/20/18.
+ */
+
+interface AnswerOption {
+    public String getAnswerText();
+    public boolean isThatRightAnswer();
+}
---
+++ app/src/main/java/com/example/csongor/quizapp/IQuizQuestion.java
@@ -0,0 +1,21 @@
+package com.example.csongor.quizapp;
+
+/**
+ * Created by csongor on 1/20/18.
+ *
+ */
+
+public interface IQuizQuestion {
+    public static final int STRING_QUESTION=0;
+    public static final int RADIO_QUESTION=1;
+    public static final int CHECKBOX_QUESTION=2;
+
+    /**
+     * @return returns the QuizQuestion Type.
+     * The AnswerOption List will determine the QuizQuestionType:
+     *                     1: The list has only 1 and only 1 AnswerOption -> STRING_QUESTION
+     *                     2: The list has 4 AnswerOption with 1 rightAnswer -> RADIO_QUESTION
+     *                     3: The list has 4 AnswerOption with multiple rightAnswer -> CHECKBOX_QUESTION
+     */
+    public int getQuestionType();
+}
