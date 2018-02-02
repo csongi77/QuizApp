013df3501b53c31b469b75ee8373804b9a7b1c46
Horváth Csongor <csongor@csbig.cslabs.local>
2018-02-02 09:11:28 +0100

    add GameState interface

---
+++ app/src/main/java/com/example/csongor/quizapp/GameState.java
@@ -0,0 +1,19 @@
+package com.example.csongor.quizapp;
+
+/**
+ * Created by csongor on 2/2/18.
+ * Through this interface can game handle different button
+ * behaviours depending on game state
+ */
+
+public interface GameState {
+    /**
+     * method for pressing right button
+     */
+    void doRightButtonAction();
+
+    /**
+     * method for pressing left button
+     */
+    void doLeftButtonAction();
+}
